ad84528dd38417a989719a348c0e955ccc32f346
Ben Vandervalk <benv@bcgsc.ca>
2014-05-20 16:52:18 -0700

    add bwa-mem.mk and smartcat utilities

---
+++ bwa-mem.mk
@@ -0,0 +1,34 @@
+#!/usr/bin/make -Rrf
+
+SHELL:=/bin/bash -o pipefail
+PATH:=/home/benv/arch/genesis/bwa-0.7.4/bin:$(PATH)
+
+ifndef queryfiles
+error::
+	$(error missing parameter 'queryfiles')
+endif
+ifndef target
+error::
+	$(error missing parameter 'target')
+endif
+ifndef name
+error::
+	$(error missing parameter 'name')
+endif
+
+default: $(name).sorted.bam.bai
+
+$(target).bwt: $(target)
+	bwa index $(target)
+
+$(name).sam.gz: $(target).bwt $(queryfiles)
+	smartcat $(queryfiles) | bwa mem $(bwa_opt) $(target) - | gzip -c > $@
+
+$(name).bam: $(name).sam.gz
+	zcat $< | samtools view -bSo $@ -
+
+$(name).sorted.bam: $(name).bam
+	samtools sort $< $(name).sorted
+
+$(name).sorted.bam.bai: $(name).sorted.bam
+	samtools index $<
